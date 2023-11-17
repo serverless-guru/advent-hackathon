fb7716a1990524c32e1392bb3c776183dc53f137
Mersad Hadzic <mersad@bind.ba>
2023-11-17 18:47:14 +0100

    retreive config

--- chatbot/src/components/molecules/add-messages-form/AddMessagesForm.tsx
+++ chatbot/src/components/molecules/add-messages-form/AddMessagesForm.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import useWebSocket from "react-use-websocket";
 import { Button } from "../../atoms/button";
-import { socketUrl } from "../chat/Chat.tsx";
 
 const AddMessagesForm: React.FC<{ clearMessagesHandle: () => void }> = (
   props
 ) => {
   const { clearMessagesHandle } = props;
+  const { socketUrl } = props;
   const [value, setValue] = useState("");
   const { sendMessage } = useWebSocket(socketUrl, {
     onOpen: () => console.log("WebSocket connection opened."),
@@ -41,7 +41,7 @@ const AddMessagesForm: React.FC<{ clearMessagesHandle: () => void }> = (
         }}
         onKeyDown={handleKeyPress}
         className={
-          "bg-[#2B2D31] w-full h-20 p-3 text-white cursor-pointer rounded-lg text-sx font-medium hover:ring-2 ring-[#826AED]    focus:outline-none   focus:ring-2 ring-[#826AED] focus:bg-black-darker placeholder-[#7E858F]"
+          "bg-[#2B2D31] w-full h-20 p-3 text-white cursor-pointer rounded-lg text-sx font-medium hover:ring-2 ring-[#826AED] focus:outline-none focus:ring-2 ring-[#826AED] focus:bg-black-darker placeholder-[#7E858F]"
         }
       />
       <div className={"flex justify-between"}>
--- chatbot/src/components/molecules/chat/Chat.tsx
+++ chatbot/src/components/molecules/chat/Chat.tsx
@@ -1,64 +1,77 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import useWebSocket from "react-use-websocket";
 import AddMessagesForm from "../add-messages-form/AddMessagesForm.tsx";
-import createApiKey from '../../../utils/api/createApiKey.ts';
-import retrieveApiConfig from '../../../utils/api/retrieveApiConfig.ts';
-import { connectWebSocket, sendWebSocketMessage } from '../../../utils/api/webSocketApi.ts';
-
-export const socketUrl = "wss://ws.ifelse.io/";
-// export const socketUrl = "wss://mmr24p5crd.execute-api.us-east-1amazonaws.com/staging";
+import retrieveApiConfig from "../../../utils/api/retrieveApiConfig.ts";
 
 const Chat: React.FC = () => {
-    const [messages, setMessages] = useState<string[]>([])
-    const {
-        lastMessage,
-        readyState,
-    } = useWebSocket(socketUrl, {
-        onOpen: () => console.log('WebSocket connection opened.'),
-        onClose: () => console.log('WebSocket connection closed.'),
-        shouldReconnect: (closeEvent) => true,
-        share: true,
-    });
+  const [messages, setMessages] = useState<string[]>([]);
+  const [socketUrl, setSocketUrl] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const config = await retrieveApiConfig();
-            console.log("API Config:", config);
-          } catch (error) {
-            console.error("Error fetching API config:", error);
-          }
-        };
+  const { lastMessage, readyState } = useWebSocket(socketUrl, {
+    onOpen: () => console.log("WebSocket connection opened."),
+    onClose: () => console.log("WebSocket connection closed."),
+    shouldReconnect: (closeEvent) => true,
+    share: true,
+  });
 
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const config = await retrieveApiConfig();
+        console.log("API Config:", config);
+        setSocketUrl(config.wsEndpoint); // Set the WebSocket URL dynamically
+      } catch (error) {
+        console.error("Error fetching API config:", error);
+      }
+    };
 
-    useEffect(() => {
-        if (readyState !== 0 && !lastMessage?.data.includes('Request served by')) {
-            const newMessages = lastMessage?.data;
-            setMessages(prevMessages => [...prevMessages, newMessages]);
+    fetchData();
+  }, []);
 
-        }
-    }, [lastMessage]);
-
-    const clearMessagesHandle = () => {
-        setMessages([]);
+  useEffect(() => {
+    if (
+      readyState !== 0 &&
+      lastMessage &&
+      !lastMessage.data.includes("Request served by")
+    ) {
+      const newMessages = lastMessage.data;
+      setMessages((prevMessages) => [...prevMessages, newMessages]);
     }
-    return (
-        <div className={'bg-[#020409] pt-20 flex flex-col items-center '}>
-            <div className={'text-white font-bold text-5xl'}>ChatBot</div>
-            <div className="w-2/4 p-6 gap-5 flex flex-col h-screen bg-[#020409] ">
-                <div className={'flex flex-col gap-[9px] h-[500px] overflow-y-auto'}>
-                    <div className={'text-white flex flex-col items-center gap-3'}>
-                        {messages.map((message) => <div key={message}
-                            className={'bg-[#2B2D31]  w-full h-auto break-words w-full  p-3 text-white  rounded-lg text-sx font-medium '}
-                        >{message}</div>)}
-                    </div>
+  }, [lastMessage, readyState]);
 
-                </div>
-                <AddMessagesForm clearMessagesHandle={clearMessagesHandle}/>
-            </div>
+  const clearMessagesHandle = () => {
+    setMessages([]);
+  };
+
+  if (!socketUrl) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={"bg-[#020409] pt-20 flex flex-col items-center "}>
+      <div className={"text-white font-bold text-5xl"}>ChatBot</div>
+      <div className="w-2/4 p-6 gap-5 flex flex-col h-screen bg-[#020409] ">
+        <div className={"flex flex-col gap-[9px] h-[500px] overflow-y-auto"}>
+          <div className={"text-white flex flex-col items-center gap-3"}>
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={
+                  "bg-[#2B2D31]  w-full h-auto break-words w-full  p-3 text-white  rounded-lg text-sx font-medium "
+                }
+              >
+                {message}
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
-export default Chat
+        {/* <AddMessagesForm
+          clearMessagesHandle={clearMessagesHandle}
+          socketUrl={socketUrl}
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
